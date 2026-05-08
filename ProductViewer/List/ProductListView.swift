67e22f27c6298344249ca276ccfbaf7e5f8c4818
Jigs <jigneshsheth@users.noreply.github.com>
2026-05-08 09:40:24 -0500

    Refactor ProductListView: Improve code formatting and structure for better readability and maintainability.

--- ProductViewer/List/ProductListView.swift
+++ ProductViewer/List/ProductListView.swift
@@ -1,79 +1,82 @@
-	//
-	//  ContentView.swift
-	//  ProductViewer
-	//
-	//  Created by Jigs Sheth on 1/27/22.
-	//
+//
+//  ContentView.swift
+//  ProductViewer
+//
+//  Created by Jigs Sheth on 1/27/22.
+//
 
 import SwiftUI
 
-	/// List of Products screen
+/// List of Products screen
 struct ProductListView: View {
-	@StateObject
-	private var productViewModel = ProductViewModelImpl(
-		productRepository: ProductRepositoryImpl(
-			remoteService: ProductServiceImpl(),
-			localStore: ProductJSONLocalStore()
-		)
-	)
-	
-	@State private var presentAlert = false
-	@State private var alertTitle = String.emptyString
-	
-	var body: some View {
-		NavigationView{
-			Group {
-				if productViewModel.productList.isEmpty && !presentAlert{
-					LoadingView(text: String.loadingProduct)
-				}else{
-					List(productViewModel.productList){ product in
-						ProductCellViewWithNavigation(product: product)
-					}
-					.listStyle(.plain)
-					.navigationTitle(String.deals)
-					.navigationBarTitleDisplayMode(.inline)
-					.refreshable {
-						await self.refreshProductData()
-					}
-				}
-			}.task {
-				await self.loadProductData()
-			}
-			.alert(alertTitle, isPresented: $presentAlert){
-				Button(String.retryTitle, role: .none, action: {
-					Task{
-						await self.loadProductData()
-					}
-				})
-			}message: {
-				Text(alertTitle)
-			}
-		}
-		
-	}
-	
-		/// Loading Product data
-	private func loadProductData() async {
-		await self.productViewModel.loadProducts()
-		if let errorMessage = productViewModel.lastErrorMessage {
-			alertTitle = errorMessage
-			presentAlert = true
-		}
-	}
-	
-	private func refreshProductData() async {
-		await self.productViewModel.refreshProducts()
-		if let errorMessage = productViewModel.lastErrorMessage {
-			alertTitle = errorMessage
-			presentAlert = true
-		}
-	}
-	
-	
+    @StateObject
+    private var productViewModel = ProductViewModelImpl(
+        productRepository: ProductRepositoryImpl(
+            remoteService: ProductServiceImpl(),
+            localStore: ProductJSONLocalStore()
+        )
+    )
+
+    @State private var presentAlert = false
+    @State private var alertTitle = String.emptyString
+
+    var body: some View {
+        NavigationView {
+            Group {
+                if productViewModel.productList.isEmpty && !presentAlert {
+                    LoadingView(text: String.loadingProduct)
+                } else {
+                    List(productViewModel.productList) { product in
+                        ProductCellViewWithNavigation(product: product)
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle(String.deals)
+                    .navigationBarTitleDisplayMode(.inline)
+                    .refreshable {
+                        await self.refreshProductData()
+                    }
+                }
+            }.task {
+                await self.loadProductData()
+            }
+            .alert(alertTitle, isPresented: $presentAlert) {
+                Button(
+                    String.retryTitle,
+                    role: .none,
+                    action: {
+                        Task {
+                            await self.loadProductData()
+                        }
+                    }
+                )
+            } message: {
+                Text(alertTitle)
+            }
+        }
+
+    }
+
+    /// Loading Product data
+    private func loadProductData() async {
+        await self.productViewModel.loadProducts()
+        if let errorMessage = productViewModel.lastErrorMessage {
+            alertTitle = errorMessage
+            presentAlert = true
+        }
+    }
+
+    private func refreshProductData() async {
+        await self.productViewModel.refreshProducts()
+        if let errorMessage = productViewModel.lastErrorMessage {
+            alertTitle = errorMessage
+            presentAlert = true
+        }
+    }
+
 }
 
 struct ProductListView_Previews: PreviewProvider {
-	static var previews: some View {
-		ProductListView()
-	}
+    static var previews: some View {
+        ProductListView()
+    }
 }
