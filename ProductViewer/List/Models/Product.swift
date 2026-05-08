29615289df67404cf66e6317f3be3f5299e6fa66
Jigs <jigneshsheth@users.noreply.github.com>
2026-05-08 08:28:23 -0500

    Updates

--- ProductViewer/List/Models/Product.swift
+++ ProductViewer/List/Models/Product.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 // MARK: - Product
-struct Product: Codable,Identifiable {
+struct Product: Codable, Identifiable {
     let id: Int
     let title, aisle, productDescription: String
     let imageURL: String
     let regularPrice: Price
     let salePrice: Price?
-    
+
     enum CodingKeys: String, CodingKey {
         case id, title, aisle
         case productDescription = "description"
@@ -25,24 +25,102 @@ struct Product: Codable,Identifiable {
 }
 
 extension Product {
-    var onSale:Bool {
-        guard let _ = self.salePrice else {
+    var onSale: Bool {
+        guard self.salePrice != nil else {
             return false
         }
         return true
     }
 }
 
-
-
 extension Product {
-	
-		/// Dummy data for validation
-	static let dummyProducts:[Product] = [
-		Product(id: 1, title: "This is Product Title.....", aisle: "B1", productDescription: "This is Product Description", imageURL: "https://picsum.photos/id/23/300/300", regularPrice:Price(amountInCents: 5000, currencySymbol: .empty, displayString: "$50.00"), salePrice: Price(amountInCents: 1500, currencySymbol: .empty, displayString: "$15.00") ),
-		Product(id: 1, title: "This is Product Title.....", aisle: "B2", productDescription: "This is Product Description", imageURL: "https://picsum.photos/id/23/300/300", regularPrice:Price(amountInCents: 5000, currencySymbol: .empty, displayString: "$50.00"), salePrice: Price(amountInCents: 1500, currencySymbol: .empty, displayString: "$15.00") ),
-		Product(id: 1, title: "This is Product Title.....", aisle: "B3", productDescription: "This is Product Description", imageURL: "https://picsum.photos/id/23/300/300", regularPrice:Price(amountInCents: 5000, currencySymbol: .empty, displayString: "$50.00"), salePrice: Price(amountInCents: 1500, currencySymbol: .empty, displayString: "$15.00") ),
-		Product(id: 1, title: "This is Product Title.....", aisle: "B4", productDescription: "This is Product Description", imageURL: "https://picsum.photos/id/23/300/300", regularPrice:Price(amountInCents: 5000, currencySymbol: .empty, displayString: "$50.00"), salePrice: Price(amountInCents: 1500, currencySymbol: .empty, displayString: "$15.00") ),
-		Product(id: 1, title: "This is Product Title.....", aisle: "B5", productDescription: "This is Product Description", imageURL: "https://picsum.photos/id/23/300/300", regularPrice:Price(amountInCents: 5000, currencySymbol: .empty, displayString: "$50.00"), salePrice: Price(amountInCents: 1500, currencySymbol: .empty, displayString: "$15.00") )
-	]
+
+    /// Dummy data for validation
+    static let dummyProducts: [Product] = [
+        Product(
+            id: 1,
+            title: "This is Product Title.....",
+            aisle: "B1",
+            productDescription: "This is Product Description",
+            imageURL: "https://picsum.photos/id/23/300/300",
+            regularPrice: Price(
+                amountInCents: 5000,
+                currencySymbol: .empty,
+                displayString: "$50.00"
+            ),
+            salePrice: Price(
+                amountInCents: 1500,
+                currencySymbol: .empty,
+                displayString: "$15.00"
+            )
+        ),
+        Product(
+            id: 1,
+            title: "This is Product Title.....",
+            aisle: "B2",
+            productDescription: "This is Product Description",
+            imageURL: "https://picsum.photos/id/23/300/300",
+            regularPrice: Price(
+                amountInCents: 5000,
+                currencySymbol: .empty,
+                displayString: "$50.00"
+            ),
+            salePrice: Price(
+                amountInCents: 1500,
+                currencySymbol: .empty,
+                displayString: "$15.00"
+            )
+        ),
+        Product(
+            id: 1,
+            title: "This is Product Title.....",
+            aisle: "B3",
+            productDescription: "This is Product Description",
+            imageURL: "https://picsum.photos/id/23/300/300",
+            regularPrice: Price(
+                amountInCents: 5000,
+                currencySymbol: .empty,
+                displayString: "$50.00"
+            ),
+            salePrice: Price(
+                amountInCents: 1500,
+                currencySymbol: .empty,
+                displayString: "$15.00"
+            )
+        ),
+        Product(
+            id: 1,
+            title: "This is Product Title.....",
+            aisle: "B4",
+            productDescription: "This is Product Description",
+            imageURL: "https://picsum.photos/id/23/300/300",
+            regularPrice: Price(
+                amountInCents: 5000,
+                currencySymbol: .empty,
+                displayString: "$50.00"
+            ),
+            salePrice: Price(
+                amountInCents: 1500,
+                currencySymbol: .empty,
+                displayString: "$15.00"
+            )
+        ),
+        Product(
+            id: 1,
+            title: "This is Product Title.....",
+            aisle: "B5",
+            productDescription: "This is Product Description",
+            imageURL: "https://picsum.photos/id/23/300/300",
+            regularPrice: Price(
+                amountInCents: 5000,
+                currencySymbol: .empty,
+                displayString: "$50.00"
+            ),
+            salePrice: Price(
+                amountInCents: 1500,
+                currencySymbol: .empty,
+                displayString: "$15.00"
+            )
+        ),
+    ]
 }
